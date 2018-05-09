8c0798f46e82311d17c55d8d3b9bb396041cef29
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-05-09 16:33:51 -0300

    Modificar o layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatIconModule } from "@angular/material/icon";
-import { MatButtonModule } from "@angular/material/button";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatMenuModule } from "@angular/material/menu";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatDividerModule,
+  MatListModule
+} from '@angular/material';
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
@@ -17,10 +24,16 @@ import { ROUTES } from "./app.routes";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatIconModule,
     MatButtonModule,
+    MatCheckboxModule,
     MatToolbarModule,
     MatMenuModule,
+    MatSidenavModule,
+    MatDividerModule,
+    MatListModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [],
