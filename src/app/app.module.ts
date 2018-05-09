fcb94e55e3ffa289104f8945d00652cce8e4e4f7
Italo Brenner Lauriano de Oliveira <italobrenner.mf@gmail.com>
2018-05-09 11:59:04 -0300

    Modificar a versão do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { MatToolbarModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatMenuModule } from "@angular/material/menu";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
 
-import { ROUTES } from './app.routes';
-import { HomeComponent } from './home/home.component';
+import { ROUTES } from "./app.routes";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    MatToolbarModule,
-    MatMenuModule,
+    BrowserAnimationsModule,
     MatIconModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
